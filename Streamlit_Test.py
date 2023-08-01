54eac8ad1f1ae9bc2bbc6ba9d6863d36b1e22127
DP92675 <mdchrist32@gmail.com>
2023-08-01 17:29:01 -0400

    Update Streamlit_Test.py

--- Streamlit_Test.py
+++ Streamlit_Test.py
@@ -22,19 +22,22 @@ filtered_data = data.iloc[start_index:end_index + 1]
 # Creating a figure and axis
 fig, ax1 = plt.subplots(figsize=(12, 6))
 
-# Plotting "Visits" on the first y-axis
-ax1.plot(filtered_data['Month'], filtered_data['Visits'], color='b', label='Visits')
+# Plotting "Visits" as a vertical bar graph
+ax1.bar(filtered_data['Month'], filtered_data['Visits'], color='b', label='Visits')
 ax1.set_xlabel('Month')
 ax1.set_ylabel('Visits', color='b')
 ax1.tick_params(axis='y', colors='b')
 ax1.legend(loc='upper left')
 
-# Creating a second y-axis to plot "Visits (y/y)"
+# Creating a second y-axis to plot "Visits (y/y)" with double line width
 ax2 = ax1.twinx()
-ax2.plot(filtered_data['Month'], filtered_data['Visits (y/y)'], color='r', label='Visits (y/y)')
-ax2.set_ylabel('Visits (y/y)', color='r')
-ax2.tick_params(axis='y', colors='r')
+ax2.plot(filtered_data['Month'], filtered_data['Visits (y/y)'] * 100, color='orange', linestyle='dotted', label='Visits (y/y)', linewidth=2)
+ax2.set_ylabel('Visits (y/y) (%)', color='orange')
+ax2.tick_params(axis='y', colors='orange')
 ax2.legend(loc='upper right')
+
+# Formatting the second y-axis as a percentage
+ax2.yaxis.set_major_formatter('{:.0f}%')
 
 # Displaying the plot in the Streamlit app
 st.pyplot(fig)
