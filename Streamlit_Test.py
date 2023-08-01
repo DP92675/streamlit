4edc4acb6aa72091b1ec91ce7451f2f8e8ef2565
DP92675 <mdchrist32@gmail.com>
2023-08-01 18:28:10 -0400

    Update Streamlit_Test.py

--- Streamlit_Test.py
+++ Streamlit_Test.py
@@ -26,7 +26,7 @@ fig = go.Figure()
 fig.add_trace(go.Bar(x=filtered_data['Month'], y=filtered_data['Visits'], name='Visits', width=20 * 86400000, marker_color='#0F1B93'))  # 20 days in milliseconds
 
 # Add a scatter trace for "Visits (y/y)" on a secondary y-axis, presented in percentages
-fig.add_trace(go.Scatter(x=filtered_data['Month'], y=filtered_data['Visits (y/y)'], name='Visits (y/y)', line=dict(dash='dot', color='#E67E22'), yaxis='y2'))
+fig.add_trace(go.Scatter(x=filtered_data['Month'], y=filtered_data['Visits (y/y)'], name='Visits (y/y)', line=dict(dash='dot', color='#E67E22', width=3), yaxis='y2'))
 
 # Update layout to include a secondary y-axis for percentages, rounded to 1 decimal, and remove grid lines
 fig.update_layout(
